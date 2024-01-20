f6ad2633f8bc78555c24ca56672eb5e69cc2339f
Rodrigo Polydoro <rspolydoro@gmail.com>
2024-01-20 12:18:54 -0300

    Removed comments

--- src/utils/Environment.ts
+++ src/utils/Environment.ts
@@ -1,7 +1,4 @@
 export const Environment = {
     SERVER_URL : import.meta.env.VITE_APP_SERVER_URL,
-    //SERVER_URL : "https://api-truco-planning.bohr.io/api",
-
-    CLIENT_URL : import.meta.env.VITE_APP_CLIENT_URL,
-    //CLIENT_URL : "https://trucoplanning.bohr.io",
+    CLIENT_URL : import.meta.env.VITE_APP_CLIENT_URL
 }
